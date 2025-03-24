c988d6f7689a65e36ce19662b3e6af37085d914b
ThisRiven <psalmurlz258@gmail.com>
2025-03-24 10:54:01 +0100

    added js from talk with teacher

---
+++ script.js
@@ -0,0 +1,8 @@
+function selectLayer1(e){
+    // document.getElementById('layer1').
+    console.log(e);
+    var img = document.getElementById('layer1');
+    img.src = e.src;
+    img.style.opacity = 1;
+    src.appendChild(img);
+}
