1bc0c6686d0c248755e1062c8e8af51c54b2c745
ThisRiven <psalmurlz258@gmail.com>
2025-03-24 11:37:21 +0100

    sadfgg

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function selectLayer1(e){
     // document.getElementById('layer1').
-    console.log(e);
-    var img = document.getElementById('layer1');
-    img.src = e.src;
-    img.style.opacity = 1;
-    src.appendChild(img);
+    // made a custom function to select the layer1 image
+    
+    console.log(e); // this will show the element that was clicked
+    var img = document.getElementById('layer1'); // get the image element
+    img.src = e.src;// set the image source to the clicked image
+    img.style.opacity = 1;// set the opacity to 1
+    src.appendChild(img);// append the image to the src div
 }
